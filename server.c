330e2324212ea5f394c247fbddc55162ed6dc6f5
ralphvw <ralphvwilliams@icloud.com>
2025-01-03 12:03:01 +0000

    write and read all

--- server.c
+++ server.c
@@ -5,11 +5,91 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <string.h>
+#include <assert.h>
 
 void die(const char *message)
 {
     perror(message);
     exit(EXIT_FAILURE);
+}
+
+const size_t k_max_msg = 4096;
+
+static int32_t one_request(int connfd)
+{
+    // 4 bytes header
+    char rbuf[4 + k_max_msg + 1];
+    int errno = 0;
+    int32_t err = read_full(connfd, rbuf, 4);
+    if (err)
+    {
+        if (errno == 0)
+        {
+            msg("EOF");
+        }
+        else
+        {
+            msg("read() error");
+        }
+        return err;
+    }
+    uint32_t len = 0;
+    memcpy(&len, rbuf, 4);
+    if (len > k_max_msg)
+    {
+        msg("too long");
+        return -1;
+    }
+    // request body
+    err = read_full(connfd, &rbuf[4], len);
+    if (err)
+    {
+        msg("read() error");
+        return err;
+    }
+
+    rbuf[4 + len] = '\0';
+    printf("client says: %s\n", &rbuf[4]);
+
+    const char reply[] = "world";
+    char wbuf[4 + sizeof(reply)];
+    len = (uint32_t)strlen(reply);
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], reply, len);
+    return write_all(connfd, wbuf, 4 + len);
+}
+
+static int32_t read_full(int fd, char *buf, size_t n)
+{
+    while (n > 0)
+    {
+        ssize_t rv = read(fd, buf, n);
+        if (rv <= 0)
+        {
+            return -1;
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+
+    return 0;
+}
+
+static int32_t write_all(int fd, const char *buf, size_t n)
+{
+    while (n > 0)
+    {
+        ssize_t rv = write(fd, buf, n);
+        if (rv <= 0)
+        {
+            return -1;
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
 }
 
 static void do_something(int connfd)
@@ -67,6 +147,15 @@ int main()
         if (connfd < 0)
         {
             continue;
+        }
+
+        while (1)
+        {
+            int32_t err = one_request(connfd);
+            if (err)
+            {
+                break;
+            }
         }
 
         do_something(connfd);
